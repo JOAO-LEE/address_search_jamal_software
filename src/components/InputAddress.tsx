f68ccef2f5a5455b8415df44b0ab6e7e456a0d97
João Lee <joaumlimaum@gmail.com>
2023-09-22 17:03:23 -0300

    Backend criado

--- src/components/InputAddress.tsx
+++ src/components/InputAddress.tsx
@@ -1,17 +1,21 @@
 import { InputLabel, Input, FormHelperText } from "@mui/material";
 import { useState } from "react";
-import SearchButton from "./Button";
+import SearchButton from "./SearchButton";
 
 export default function AddressInput () {
   let [addressInput, setAddressInput] = useState("");
+  console.log(addressInput);
+  
 
   let addressRegexExpression = new RegExp(/^\d{8}$/);
   let isInputACep = addressRegexExpression.test(addressInput);
 
+  let inputErrorFeedback = (!isInputACep && ( addressInput.length >= 1 && addressInput.length < 8));
+
   return (
     <InputLabel>
-      <Input placeholder='Insira o cep aqui! Ex: "00000000"' error={!isInputACep} onChange={(e) => setAddressInput(e.target.value)} value={addressInput} aria-describedby="component-error-text"/>
-      {(!isInputACep && addressInput.length < 8) && <FormHelperText id="component-error-text">O CEP precisa ser no formato "00000000"!</FormHelperText>}
+      <Input placeholder='Insira o cep aqui! Ex: "00000000"' error={inputErrorFeedback} onChange={(e) => setAddressInput(e.target.value)} value={addressInput} aria-describedby="component-error-text"/>
+      {inputErrorFeedback && <FormHelperText id="component-error-text">O CEP precisa ser no formato "00000000"!</FormHelperText>}
       <SearchButton isButtonEnabled={isInputACep}/>
       </InputLabel>
   )
